14ebcb59ff2ecff39ebe7f4bd74c2fc27c77102a
Carl Timothy Ligutom <carl.timothy.ligutom@student.uno-r.edu.ph>
2025-10-20 06:10:11 +0000

    Upload Initial Code

---
+++ Dino Discover.py
@@ -0,0 +1,14 @@
+import pygame
+import time 
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Test #1")
+clock = pygame.time.Clock()
+running = True
+
+
+
+"""    pygame.display.flip()
+    clock.tick(60)
+pygame.quit()"""
