7bb53dbad799902654a01cd029eb5b67226a6155
Carl Timothy Ligutom <carl.timothy.ligutom@student.uno-r.edu.ph>
2025-12-02 07:27:24 +0000

    removed debug code

--- Dino Discover.py
+++ Dino Discover.py
@@ -1,7 +1,7 @@
 import pygame
 
 pygame.init()
-icon = pygame.image.load("assets\Cute-Dinosaur-Left.png")
+icon = pygame.image.load("D:/output/assets/Cute-Dinosaur-Left.png")
 pygame.display.set_icon(icon)
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Dino Discovery!")
@@ -17,15 +17,15 @@ button_font = pygame.font.Font(None, 40)
 role_font = pygame.font.SysFont(None, 44)
 creds_button_font = pygame.font.SysFont(None, 40)
 settings_button_font = pygame.font.Font(None, 40)
-bg_img = pygame.image.load('assets\menu.jpeg').convert()
+bg_img = pygame.image.load('D:/output/assets/menu.jpeg').convert()
 bg_img = pygame.transform.scale(bg_img, (800, 600))
-dino_img = pygame.image.load('assets\Cute-Dinosaur.png').convert_alpha()
-dino_left_img = pygame.image.load('assets\Cute-Dinosaur-Left.png').convert_alpha()
-nugget_img = pygame.image.load('assets\nuggies.webp').convert_alpha()
-button_bg = pygame.image.load('assets\button-large.png').convert_alpha()
-main_ui_img = pygame.image.load('assets\main-ui.png').convert_alpha()
-check_img = pygame.image.load('assets\check.gif').convert_alpha()
-x_img = pygame.image.load('assets\x.gif').convert_alpha()
+dino_img = pygame.image.load('D:/output/assets/Cute-Dinosaur.png').convert_alpha()
+dino_left_img = pygame.image.load('D:/output/assets/Cute-Dinosaur-Left.png').convert_alpha()
+nugget_img = pygame.image.load('D:/output/assets/nuggies.webp').convert_alpha()
+button_bg = pygame.image.load('D:/output/assets/button-large.png').convert_alpha()
+main_ui_img = pygame.image.load('D:/output/assets/main-ui.png').convert_alpha()
+check_img = pygame.image.load('D:/output/assets/check.gif').convert_alpha()
+x_img = pygame.image.load('D:/output/assets/x.gif').convert_alpha()
 dino_img = pygame.transform.scale(dino_img, (500, 500))
 dino_left_img = pygame.transform.scale(dino_left_img, (500, 500))
 nugget_img = pygame.transform.scale(nugget_img, (100, 90))
@@ -33,7 +33,7 @@ button_bg = pygame.transform.scale(button_bg, (150, 60))
 main_ui_img = pygame.transform.scale(main_ui_img, (600, 350))
 check_img = pygame.transform.scale(check_img, (80, 80))
 x_img = pygame.transform.scale(x_img, (80, 80))
-pygame.mixer.music.load("assets\bg_music.mp3")
+pygame.mixer.music.load("D:/output/assets/bg_music.mp3")
 pygame.mixer.music.set_volume(0.5)
 pygame.mixer.music.play(-1)
 dino_rect = dino_img.get_rect(topleft=(550, 200))
@@ -389,8 +389,8 @@ while running:
                     dragging_volume = True
                 elif creds_btn.collidepoint(event.pos):
                     in_creds = True
-                    "Debug credits button"
-                    print(f"Credits Clicked at {event.pos}")
+                    """Debug credits button"
+                    print(f"Credits Clicked at {event.pos}")"""
             elif in_difficulty:
                 back_btn = pygame.Rect(800 / 2 - 75, 450, 150, 60)
                 if back_btn.collidepoint(event.pos):
