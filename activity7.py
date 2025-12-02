6de22354451e7966e58f4ecc37fe9f35197dd6fa
Carl Timothy Ligutom <carl.timothy.ligutom@student.uno-r.edu.ph>
2025-12-02 06:36:50 +0000

    test

--- activity7.py
+++ activity7.py
@@ -11,23 +11,21 @@ def login():
     print("===LOGIN===")
     inp = input("Enter username: ").strip()
     inpp = input("Enter password: ").strip()
-    user = open("user.txt", "r")
-    users = [u.strip() for u in user.readlines()]
-    user.close()
+    user_file = open("user.txt", "r")
     pass_file = open("pass.txt", "r")
-    passwords = [p.strip() for p in pass_file.readlines()]
-    pass_file.close()
-    if inp in users:
-        index = users.index(inp)
-        if users.index(inp) < len(passwords) and passwords[index] == inpp:
-            print("")
+    if inp == user_file.readline().strip():
+        pass_file.readline()
+        if inpp == pass_file.readline().strip():
+            print("Login Successful!")
         else:
-            print("")
+            print("Wrong password.")
             sys.exit()
     else:
-        print("")
+        print("Username not found.")
         sys.exit()
-
+    user_file.close()
+    pass_file.close()
+    
 def signup():
     print("===REGISTRATION===")
     username = input("Enter a username: ").strip()
@@ -54,44 +52,3 @@ def Main():
             sys.exit()
 
 Main()
-
-
-"""
-def login():
-    print("===LOGIN===")
-    inp = input("Enter username: ").strip()
-    inpp = input("Enter password: ").strip()
-
-    user_file = open("user.txt", "r")
-    pass_file = open("pass.txt", "r")
-
-    users = user_file.readlines()
-    passwords = pass_file.readlines()
-
-    user_file.close()
-    pass_file.close()
-
-    clean_users = []
-    clean_pass = []
-
-    for u in users:
-        clean_users.append(u.strip())
-
-    for p in passwords:
-        clean_pass.append(p.strip())
-
-    found = False
-    for i in range(len(clean_users)):
-        if clean_users[i] == inp:
-            found = True
-            if clean_pass[i] == inpp:
-                print("Login Successful!")
-                return
-            else:
-                print("Wrong password.")
-                sys.exit()
-
-    if not found:
-        print("Username not found.")
-        sys.exit()
-"""
